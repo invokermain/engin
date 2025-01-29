df31de15ff24f248b89819fa05601a3ce3f20306
= <timothy725@googlemail.com>
2025-01-29 08:33:03 +0000

    make mypy happy

--- src/engin/_lifecycle.py
+++ src/engin/_lifecycle.py
@@ -2,7 +2,7 @@ import asyncio
 import logging
 from contextlib import AbstractAsyncContextManager, AbstractContextManager
 from types import TracebackType
-from typing import TypeAlias
+from typing import TypeAlias, TypeGuard, cast
 
 LOG = logging.getLogger("engin")
 
@@ -59,7 +59,7 @@ class Lifecycle:
         suppressed_cm = _AExitSuppressingAsyncContextManager(cm)
         self._context_managers.append(suppressed_cm)
 
-    def list(self) -> list[_AnyContextManager]:
+    def list(self) -> list[AbstractAsyncContextManager]:
         """
         List all the defined tasks.
 
@@ -72,13 +72,12 @@ class Lifecycle:
 class _AExitSuppressingAsyncContextManager(AbstractAsyncContextManager):
     def __init__(self, cm: _AnyContextManager) -> None:
         self._cm = cm
-        self._is_async = hasattr(cm, "__aenter__")
 
     async def __aenter__(self) -> None:
-        if self._is_async:
+        if self._is_async_cm(self._cm):
             await self._cm.__aenter__()
         else:
-            await asyncio.to_thread(self._cm.__enter__)
+            await asyncio.to_thread(cast(AbstractContextManager, self._cm).__enter__)
 
     async def __aexit__(
         self,
@@ -88,9 +87,18 @@ class _AExitSuppressingAsyncContextManager(AbstractAsyncContextManager):
         /,
     ) -> None:
         try:
-            if self._is_async:
+            if self._is_async_cm(self._cm):
                 await self._cm.__aexit__(exc_type, exc_value, traceback)
             else:
-                await asyncio.to_thread(self._cm.__exit__, exc_type, exc_value, traceback)
+                await asyncio.to_thread(
+                    cast(AbstractContextManager, self._cm).__exit__,
+                    exc_type,
+                    exc_value,
+                    traceback,
+                )
         except Exception as err:
             LOG.error("error in lifecycle hook stop, ignoring...", exc_info=err)
+
+    @staticmethod
+    def _is_async_cm(cm: _AnyContextManager) -> TypeGuard[AbstractAsyncContextManager]:
+        return hasattr(cm, "__aenter__")
