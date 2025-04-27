dc464435b995a771c93ab0bebf606af58d8a8583
github-action-benchmark <github@users.noreply.github.com>
2025-04-27 12:33:50 +0000

    add Benchmark (pytest) benchmark result for 4bc0ab7d018402336d1554e12049027e2afc8801

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745756658252,
+  "lastUpdate": 1745757230080,
   "repoUrl": "https://github.com/invokermain/engin",
   "entries": {
     "Benchmark": [
@@ -29,6 +29,36 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000063813843322612",
             "extra": "mean: 2.234620202400089 msec\nrounds: 5000"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "timothy725@googlemail.com",
+            "name": "="
+          },
+          "committer": {
+            "email": "44192816+invokermain@users.noreply.github.com",
+            "name": "Tim OSullivan",
+            "username": "invokermain"
+          },
+          "distinct": true,
+          "id": "4bc0ab7d018402336d1554e12049027e2afc8801",
+          "message": "check.yaml: run tests on multiple os",
+          "timestamp": "2025-04-27T13:33:02+01:00",
+          "tree_id": "1773c0172b39c6e3383972dd6053b6a09b85aada",
+          "url": "https://github.com/invokermain/engin/commit/4bc0ab7d018402336d1554e12049027e2afc8801"
+        },
+        "date": 1745757229678,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/benchmarks/test_bench_assembler.py::test_assembler_benchmark",
+            "value": 419.82162347322026,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000053678191125322156",
+            "extra": "mean: 2.381964015399956 msec\nrounds: 5000"
           }
         ]
       }
