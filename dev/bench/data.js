72a397194ace18f448a71e8bc13f35a647dd57e1
github-action-benchmark <github@users.noreply.github.com>
2025-04-27 12:24:18 +0000

    add Benchmark (pytest) benchmark result for 3d901d0b4465d7b822de5700e4b01dea97350b8d

---
+++ dev/bench/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1745756658252,
+  "repoUrl": "https://github.com/invokermain/engin",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "timothy725@googlemail.com",
+            "name": "="
+          },
+          "committer": {
+            "email": "timothy725@googlemail.com",
+            "name": "="
+          },
+          "distinct": true,
+          "id": "3d901d0b4465d7b822de5700e4b01dea97350b8d",
+          "message": "benchmark.yaml: change token",
+          "timestamp": "2025-04-27T13:23:33+01:00",
+          "tree_id": "52275ee503a0423cbde68af3433a4c04ce903341",
+          "url": "https://github.com/invokermain/engin/commit/3d901d0b4465d7b822de5700e4b01dea97350b8d"
+        },
+        "date": 1745756657861,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/benchmarks/test_bench_assembler.py::test_assembler_benchmark",
+            "value": 447.50333811801767,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000063813843322612",
+            "extra": "mean: 2.234620202400089 msec\nrounds: 5000"
+          }
+        ]
+      }
+    ]
+  }
+}
